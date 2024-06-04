658488d9e784994405cefa0adc1c52a83ab15c90
rivkyschon <rivkyschon@outlook.co.il>
2024-06-04 16:40:55 +0300

    developing..

--- cloud_infrastructure/main.tf
+++ cloud_infrastructure/main.tf
@@ -1,14 +1,5 @@
 # main.tf
 
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 4.0"  # Or your preferred version
-    }
-  }
-}
-
 provider "google" {
   project     = var.project_id
   region      = var.region
