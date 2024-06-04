c31192b7e905c5f23a29c86322595cf781c6324c
rivkyschon <rivkyschon@outlook.co.il>
2024-06-04 16:44:39 +0300

    developing..

--- cloud_infrastructure/main.tf
+++ cloud_infrastructure/main.tf
@@ -1,9 +1,23 @@
 # main.tf
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"   # Or your preferred version
+    }
+  }
+}
+
+provider "google" {
+  project     = var.project_id
+  region      = var.region
+}
+
 module "network" {
   source                  = "./modules/network"
   vpc_name                = var.vpc_name
   region                  = var.region
   cloud_run_subnet_cidr   = var.cloud_run_subnet_cidr
   lb_subnet_cidr          = var.lb_subnet_cidr
-  connector_cidr_range    = var.connector_cidr_range # If using VPC Connector
 }
