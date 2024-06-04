d6684518b1628e83dce0bbc16353f1f4a3ef1973
rivkyschon <rivkyschon@outlook.co.il>
2024-06-04 16:38:50 +0300

    developing..

--- cloud_infrastructure/main.tf
+++ cloud_infrastructure/main.tf
@@ -1,18 +1,24 @@
-module "vpc" {
-  source = "./modules/vpc"
-  project_id = var.project_id
-  region     = var.region
-  network_name = "my-vpc-network"
-  subnets = [
-    {
-      subnet_name   = "subnet-1"
-      subnet_ip     = "10.0.1.0/24"
-      subnet_region = var.region
-    },
-    {
-      subnet_name   = "subnet-2"
-      subnet_ip     = "10.0.2.0/24"
-      subnet_region = var.region
+# main.tf
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"  # Or your preferred version
     }
-  ]
+  }
+}
+
+provider "google" {
+  project     = var.project_id
+  region      = var.region
+}
+
+module "network" {
+  source                  = "./modules/network"
+  vpc_name                = var.vpc_name
+  region                  = var.region
+  cloud_run_subnet_cidr   = var.cloud_run_subnet_cidr
+  lb_subnet_cidr          = var.lb_subnet_cidr
+  connector_cidr_range    = var.connector_cidr_range # If using VPC Connector
 }
--- cloud_infrastructure/modules/vpc/output.tf
+++
@@ -1,7 +0,0 @@
-output "network_name" {
-  value = google_compute_network.vpc_network.name
-}
-
-output "subnets" {
-  value = google_compute_subnetwork.subnet
-}
