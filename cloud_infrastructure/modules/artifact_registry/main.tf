650cf0f56f6fa6d105189649268164f6dfc62ea2
rivkyschon <rivkyschon@outlook.co.il>
2024-06-05 04:30:57 +0300

    developing..

--- cloud_infrastructure/modules/artifact_registry/main.tf
+++ cloud_infrastructure/modules/artifact_registry/main.tf
@@ -1,5 +1,4 @@
 resource "google_artifact_registry_repository" "docker_repo" {
-  provider    = google-beta
   location    = var.location
   repository_id = var.repository_id
   description = var.description
