653ee9129020c3ab9bba9105b6db8f60fd548e12
rivkyschon <rivkyschon@outlook.co.il>
2024-06-05 04:31:30 +0300

    developing..

--- cloud_infrastructure/modules/artifact_registry/main.tf
+++ cloud_infrastructure/modules/artifact_registry/main.tf
@@ -6,29 +6,3 @@ resource "google_artifact_registry_repository" "docker_repo" {
 
   kms_key_name = var.kms_key_name
 }
-
-
-# IAM Policy for Artifact Registry
-data "google_iam_policy" "artifact_registry_policy" {
-  binding {
-    role = "roles/artifactregistry.writer"
-
-    members = var.writer_members
-    
-  }
-
-  binding {
-    role = "roles/artifactregistry.reader"
-
-    members = var.reader_members
-    
-  }
-}
-
-resource "google_artifact_registry_repository_iam_policy" "policy" {
-  provider    = google-beta
-  project     = google_artifact_registry_repository.docker_repo.project
-  location    = google_artifact_registry_repository.docker_repo.location
-  repository  = google_artifact_registry_repository.docker_repo.name
-  policy_data = data.google_iam_policy.artifact_registry_policy.policy_data
-}
