d07bca04da8c1a6013022f6e29ffb1d322457fed
rivkyschon <rivkyschon@outlook.co.il>
2024-06-05 14:44:21 +0300

    developing..

--- cloud_infrastructure/modules/cloud_run/main.tf
+++ cloud_infrastructure/modules/cloud_run/main.tf
@@ -14,7 +14,7 @@ resource "google_cloud_run_service" "default" {
 
 resource "google_cloud_run_service_iam_member" "member" {
   location = google_cloud_run_service.default.location
-  project  = google_cloud_run_service.default.project_id
+  project  = var.project_id
   service  = google_cloud_run_service.default.name
   role     = "roles/run.invoker"
   member   = "allUsers"
