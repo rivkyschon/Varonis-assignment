18351f3202a12ff92c202377ddefe7db3263b29c
rivkyschon <rivkyschon@outlook.co.il>
2024-06-05 03:47:57 +0300

    developing..

--- cloud_infrastructure/variables.tf
+++ cloud_infrastructure/variables.tf
@@ -39,5 +39,41 @@ variable "load_balancer_name" {
   description = "Name of the internal load balancer"
 }
 
+variable "location" {
+  type        = string
+  description = "The location where the repository will be created (e.g., 'us-central1')."
+  default     = "us-central1"
+}
+
+variable "repository_id" {
+  type        = string
+  description = "The ID of the repository (e.g., 'my-docker-repo')."
+}
+
+variable "description" {
+  type        = string
+  description = "The description of the repository."
+  default     = "Docker repository for my application"
+}
+
+variable "kms_key_name" {
+  type        = string
+  description = "The Cloud KMS key name for encrypting the repository data."
+  default     = null  # Use Google-managed keys if not specified
+}
+
+variable "writer_members" {
+  type        = list(string)
+  description = "The list of members who will have write access to the repository."
+  default     = []
+}
+
+variable "reader_members" {
+  type        = list(string)
+  description = "The list of members who will have read access to the repository."
+  default     = []
+}
+
+
 # Add more variables here if you have additional ones in your terraform.tfvars file
 # ... (other variables if applicable)
