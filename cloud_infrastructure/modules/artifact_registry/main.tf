917004541bb3edb4923041aa9e3aa0649623f0a6
rivkyschon <rivkyschon@outlook.co.il>
2024-06-05 04:43:43 +0300

    developing..

--- cloud_infrastructure/modules/artifact_registry/main.tf
+++ cloud_infrastructure/modules/artifact_registry/main.tf
@@ -12,7 +12,7 @@ resource "google_artifact_registry_repository" "docker_repo" {
 
 
 resource "google_kms_crypto_key_iam_member" "crypto_key" {
-  crypto_key_id = module.kms.key_name
+  crypto_key_id = "key_ring"
   role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
   member        = "users:rivky.schon@grunitech.com"
 }
